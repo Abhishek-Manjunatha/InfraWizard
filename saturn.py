db7a1545e91a4128d3afde802a20f746d4c39407
Abhishek Manjunath <abhishekgm.in@gmail.com>
2024-07-19 23:43:06 +0530

    added Saturn state

--- saturn.py
+++ saturn.py
@@ -2,3 +2,6 @@ Saturn has many moons but Titan is the largest moon in the entire solar system.
 
 
 Also, Saturn is the jewel crown of the Solar system which makes saturn the beatifull planet in the Solar System.
+
+saturn is a gas giant
+
